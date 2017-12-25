171c272f48941debdb21f597ad3458a250e8a29d
micyo202 <micyo202@163.com>
2017-12-25 18:33:44 +0800

    添加安全存储基本方法

--- TaxGeneralPlus/Classes/Base/Util/Security/BaseSecurityUtil.h
+++ TaxGeneralPlus/Classes/Base/Util/Security/BaseSecurityUtil.h
@@ -1,13 +1,27 @@
-//
-//  BaseSecurityUtil.h
-//  TaxGeneralPlus
-//
-//  Created by Apple on 2017/12/25.
-//  Copyright © 2017年 prient. All rights reserved.
-//
+/************************************************************
+ Class    : BaseSecurityUtil.h
+ Describe : 安全存储类，采用Keychain存储数据
+ Company  : Prient
+ Author   : Yanzheng 严正
+ Date     : 2017-12-25
+ Version  : 1.0
+ Declare  : Copyright © 2017 Yanzheng. All rights reserved.
+ ************************************************************/
 
 #import <Foundation/Foundation.h>
 
 @interface BaseSecurityUtil : NSObject
+
+// 单例模式方法
+SingletonH(BaseSecurityUtil)
+
+// 保存用户基本信息
+- (void)saveUserDict:(NSDictionary *)dict;
+
+// 获取用户基本信息
+- (NSDictionary *)getUserDict;
+
+// 删除用户信息
+- (BOOL)removeUserDict;
 
 @end
--- TaxGeneralPlus/Classes/Base/Util/Security/BaseSecurityUtil.m
+++ TaxGeneralPlus/Classes/Base/Util/Security/BaseSecurityUtil.m
@@ -1,13 +1,30 @@
-//
-//  BaseSecurityUtil.m
-//  TaxGeneralPlus
-//
-//  Created by Apple on 2017/12/25.
-//  Copyright © 2017年 prient. All rights reserved.
-//
+/************************************************************
+ Class    : BaseSecurityUtil.m
+ Describe : 安全存储类，采用Keychain存储数据
+ Company  : Prient
+ Author   : Yanzheng 严正
+ Date     : 2017-12-25
+ Version  : 1.0
+ Declare  : Copyright © 2017 Yanzheng. All rights reserved.
+ ************************************************************/
 
 #import "BaseSecurityUtil.h"
 
 @implementation BaseSecurityUtil
+
+#pragma mark - 单例模式方法
+SingletonM(BaseSecurityUtil)
+
+- (void)saveUserDict:(NSDictionary *)dict {
+    
+}
+
+- (NSDictionary *)getUserDict {
+    return nil;
+}
+
+- (BOOL)removeUserDict {
+    return nil;
+}
 
 @end
