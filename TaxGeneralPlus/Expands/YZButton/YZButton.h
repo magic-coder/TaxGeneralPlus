0ccf4c6e2204d47bd90b2c3d658f56e937730f4b
micyo202 <micyo202@163.com>
2017-12-28 17:53:00 +0800

    添加自扩展布局按钮组件

--- TaxGeneralPlus/Expands/YZButton/YZButton.h
+++ TaxGeneralPlus/Expands/YZButton/YZButton.h
@@ -1,13 +1,18 @@
-//
-//  YZButton.h
-//  TaxGeneralPlus
-//
-//  Created by Apple on 2017/12/28.
-//  Copyright © 2017年 prient. All rights reserved.
-//
+/************************************************************
+ Class    : YZButton.h
+ Describe : 自定义文字、图片混合按钮样式，可进行图片、文字上、下、左、右排版布局
+ Company  : Prient
+ Author   : Yanzheng 严正
+ Date     : 2017-12-28
+ Version  : 1.0
+ Declare  : Copyright © 2017 Yanzheng. All rights reserved.
+ ************************************************************/
 
 #import <UIKit/UIKit.h>
 
 @interface YZButton : UIButton
+
+@property (nonatomic,assign) CGRect titleRect;
+@property (nonatomic,assign) CGRect imageRect;
 
 @end
--- TaxGeneralPlus/Expands/YZButton/YZButton.m
+++ TaxGeneralPlus/Expands/YZButton/YZButton.m
@@ -1,21 +1,29 @@
-//
-//  YZButton.m
-//  TaxGeneralPlus
-//
-//  Created by Apple on 2017/12/28.
-//  Copyright © 2017年 prient. All rights reserved.
-//
+/************************************************************
+ Class    : YZButton.m
+ Describe : 自定义文字、图片混合按钮样式，可进行图片、文字上、下、左、右排版布局
+ Company  : Prient
+ Author   : Yanzheng 严正
+ Date     : 2017-12-28
+ Version  : 1.0
+ Declare  : Copyright © 2017 Yanzheng. All rights reserved.
+ ************************************************************/
 
 #import "YZButton.h"
 
 @implementation YZButton
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (CGRect)titleRectForContentRect:(CGRect)contentRect{
+    if (!CGRectIsEmpty(self.titleRect) && !CGRectEqualToRect(self.titleRect, CGRectZero)) {
+        return self.titleRect;
+    }
+    return [super titleRectForContentRect:contentRect];
 }
-*/
+
+- (CGRect)imageRectForContentRect:(CGRect)contentRect{
+    if (!CGRectIsEmpty(self.imageRect) && !CGRectEqualToRect(self.imageRect, CGRectZero)) {
+        return self.imageRect;
+    }
+    return [super imageRectForContentRect:contentRect];
+}
 
 @end
