1469bf76b7f3bfe403d326662d315fc499d38cc5
micyo202 <micyo202@163.com>
2018-02-28 11:12:37 +0800

    更新测试模块

--- TaxGeneralPlus/Classes/Test/Controller/TestViewController.h
+++ TaxGeneralPlus/Classes/Test/Controller/TestViewController.h
@@ -8,8 +8,8 @@
  Declare  : Copyright © 2017 Yanzheng. All rights reserved.
  ************************************************************/
 
-#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
 
-@interface TestViewController : UIViewController
+@interface TestViewController : BaseViewController
 
 @end
