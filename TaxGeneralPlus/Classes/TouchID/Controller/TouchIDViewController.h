d2b65998b710bd31732c5ba0e9cc1ddfa5dace39
micyo202 <micyo202@163.com>
2018-02-28 11:14:57 +0800

    优化指纹解锁模块

--- TaxGeneralPlus/Classes/TouchID/Controller/TouchIDViewController.h
+++ TaxGeneralPlus/Classes/TouchID/Controller/TouchIDViewController.h
@@ -8,8 +8,8 @@
  Declare  : Copyright © 2017 Yanzheng. All rights reserved.
  ************************************************************/
 
-#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
 
-@interface TouchIDViewController : UIViewController
+@interface TouchIDViewController : BaseViewController
 
 @end
