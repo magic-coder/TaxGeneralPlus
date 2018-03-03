d641c01489fe7b42264eb32abdd91eb3106bc928
micyo202 <micyo202@163.com>
2018-03-03 11:30:33 +0800

    更新基本的Web视图控制器

--- TaxGeneralPlus/Classes/Base/Controller/Web/BaseWebViewController.h
+++ TaxGeneralPlus/Classes/Base/Controller/Web/BaseWebViewController.h
@@ -8,9 +8,9 @@
  Declare  : Copyright © 2017 Yanzheng. All rights reserved.
  ************************************************************/
 
-#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
 
-@interface BaseWebViewController : UIViewController
+@interface BaseWebViewController : BaseViewController
 
 - (instancetype)initWithURL:(NSString *)url;
 
