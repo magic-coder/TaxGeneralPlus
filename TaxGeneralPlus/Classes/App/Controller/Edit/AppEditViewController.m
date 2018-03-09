f5892a3c05443dc3598e85fd4c4b960666ade418
micyo202 <micyo202@163.com>
2018-03-09 15:34:18 +0800

    更新应用管理器导航栏切换效果

--- TaxGeneralPlus/Classes/App/Controller/Edit/AppEditViewController.m
+++ TaxGeneralPlus/Classes/App/Controller/Edit/AppEditViewController.m
@@ -71,6 +71,16 @@ static NSString * const reuseHeaderIdentifier = @"reuseHeaderIdentifier";
     [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleDefault;// 设置顶部状态栏字体为黑色
     self.navigationController.navigationBar.tintColor = DEFAULT_BLUE_COLOR;// 设置导航栏itemBar字体颜色
     self.navigationController.navigationBar.titleTextAttributes = @{ NSForegroundColorAttributeName : [UIColor blackColor] };// 设置导航栏title标题字体颜色
+    self.jz_navigationBarTintColor = [UIColor whiteColor];
+}
+
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    
+    [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;// 设置顶部状态栏字体颜色为白色
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];// 设置导航栏itemBar字体颜色
+    self.navigationController.navigationBar.titleTextAttributes = @{ NSForegroundColorAttributeName : [UIColor whiteColor] };// 设置导航栏title标题字体颜色
+    self.jz_navigationBarTintColor = DEFAULT_BLUE_COLOR;
 }
 
 #pragma mark - 初始化加载数据
